68413a6488435306b80056772fe5e6f809fa2323
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-03-13 12:02:28 -0700

    steps added

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Step 1:
+// add eventListener to both left and right buttons and make sure it works when you click them
+// HINT: work inside a function
+// HINT: create an array of background image names inside the function
+// HINT: Use loop
+
+// Step 2:
+// add auto image player
+// there are 2 buttons for play and stop
+// When i hit play buttons image slider should work automatically and stop when i hit stop button
+// HINT: setInterval and clearInterval functions
+
+// Step 3:
+// create names for each car in the image and show them on top of slider in order
+
