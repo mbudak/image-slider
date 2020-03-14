01a86a8255bc1bc329473d51847715f18f2ee109
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-03-13 20:10:30 -0700

    last commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,67 @@
-// Step 1:
-// add eventListener to both left and right buttons and make sure it works when you click them
-// HINT: work inside a function
-// HINT: create an array of background image names inside the function
-// HINT: Use loop
+// IIFE 
+(function() {
+  // array of our background image names
+  const pictures = [ "contBcg-0", "contBcg-1", "contBcg-2", "contBcg-3", "contBcg-4"];
+  const carNames = [ "Mercedes AMG", "Mercedes E Class", "BMW", "AUDI", "Dodge Challenger"];
 
-// Step 2:
-// add auto image player
-// there are 2 buttons for play and stop
-// When i hit play buttons image slider should work automatically and stop when i hit stop button
-// HINT: setInterval and clearInterval functions
+  //select both left and right button. Add event listeners
+  const buttons = document.querySelectorAll('.btn')
+  // image container
+  const imageDIV = document.querySelector('.img-container');
 
-// Step 3:
-// create names for each car in the image and show them on top of slider in order
+  // play, stop buttons
+  const playBtn = document.querySelector('#playBtn');
+  const stopBtn = document.querySelector('#stopBtn');
 
-// Step 4:
-// push to github pages
+  // car name 
+  const carName = document.querySelector('.car-name');
+  carName.textContent = carNames[0];
+
+  let counter = 0;
+  var allIntervals = [];
+ 
+  // auto slider
+  playBtn.addEventListener('click', function() {
+    var timer = setInterval(function() {
+
+    counter++;
+    if(counter > pictures.length -1){
+      counter = 0;
+    }
+    imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+    carName.textContent = carNames[counter];
+    // pushing all intervals
+    allIntervals.push(timer);
+
+    }, 1500)
+    
+  })
+  stopBtn.addEventListener('click', function() {
+    // clearing all intervals
+    allIntervals.forEach(clearInterval);
+
+    });
+
+  // manual slider
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+      if (button.classList.contains('btn-left')){
+        console.log(button.classList);
+        counter--;
+        if(counter < 0){
+          counter = pictures.length -1;
+        }
+        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+        carName.textContent = carNames[counter];
+      }
+      if (button.classList.contains('btn-right')){
+        counter++;
+        if(counter > pictures.length -1){
+          counter = 0;
+        }
+        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+        carName.textContent = carNames[counter];
+      }
+    })
+  })
+})();
